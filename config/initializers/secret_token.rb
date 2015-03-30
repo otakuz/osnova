32a7ee6ad4f72617047710ec71f9bd6dd50f20e7
Artur Pevnev <artursmg@msn.com>
2015-03-30 10:49:17 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Osnova::Application.config.secret_key_base = '9bfc196a649da5e3fb9c2c19601ac3fe2d8f6c8907194f98739a1cc5aed3a086522bfda3bdc3c12d450b14a45ab32a4a23220d3d0b7ef42e598dbeaaea607f6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Osnova::Application.config.session_store :cookie_store, key: '_osnova_session'
